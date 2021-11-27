4958b43863ac087555e9a6efe88b82f6ece78148
Michelle Lucero <iammichellelucero@gmail.com>
2021-11-27 00:30:20 -0500

    added alerts for win and tie cases

--- js/Game.js
+++ js/Game.js
@@ -1,14 +1,12 @@
 export default class Game {
-  constructor(squares, playerX, playerO) {
+  constructor(squares) {
+    this.isGameOver = false;
     this.turn = 'x';
     this.squares = squares;
-    // player1 will always be x
-    this.playerX = playerX;
-    this.playerO = playerO;
     this.squares.forEach((square) => {
       // console.log(square);
       square.addEventListener('click', (event) => {
-        if (this.isSquareEmpty(event)) {
+        if (this.isSquareEmpty(event) && !this.isGameOver) {
           this.updateBoard(event);
         }
       });
@@ -20,11 +18,53 @@ export default class Game {
   updateTurn() {
     this.turn === 'x' ? (this.turn = 'o') : (this.turn = 'x');
   }
+  checkWin(playerType) {
+    const winningCombos = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    winningCombos.forEach((combos) => {
+      const [index1, index2, index3] = combos;
+      if (
+        this.squares[index1].textContent === playerType &&
+        this.squares[index2].textContent === playerType &&
+        this.squares[index3].textContent === playerType
+      ) {
+        alert(`${playerType} won`);
+        this.isGameOver = true;
+        return true;
+      }
+      return false;
+    });
+  }
+  isBoardFull() {
+    const totalSquares = this.squares.length;
+    let count = 0;
+    this.squares.forEach((square) => {
+      if (square.textContent != '') {
+        count += 1;
+      }
+    });
+    this.isFull = totalSquares === count;
+  }
   updateBoard(event) {
     if (this.turn === 'x') {
       event.target.textContent = 'x';
+      this.checkWin('x');
     } else {
       event.target.textContent = 'o';
+      this.checkWin('o');
+    }
+    this.isBoardFull();
+    if (this.isFull) {
+      alert('tie');
+      this.isGameOver = true;
     }
     this.updateTurn();
   }
--- js/main.js
+++ js/main.js
@@ -2,9 +2,9 @@ import Game from './Game.js';
 import Player from './Player.js';
 
 const squares = document.querySelectorAll('.square');
-const playerX = new Player('x');
-const playerO = new Player('o');
-const game = new Game(squares, playerX, playerO);
+// const playerX = new Player('x');
+// const playerO = new Player('o');
+const game = new Game(squares);
 console.log('main');
 
 // const hello = (e) => {
